22ec5e5238060a9e490ccbdcd1dc6082ec3e94b0
Stratienko <artemstratienko94@gmail.com>
2022-05-14 13:48:22 +0200

    Initial project setup

---
+++ next-i18next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  i18n: {
+    defaultLocale: "common",
+    locales: ["common"],
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [],
+  theme: {
+    extend: {
+      maxWidth: {
+        360: "90rem",
+      },
+      width: {
+        360: "90rem",
+      },
+    },
+  },
+};
